43cb11beeedc663c19a627134de0170ebbe44f63
seeynt <61554700+seeynt@users.noreply.github.com>
2020-03-23 16:52:15 +0300

    Add files via upload

--- 03/main.cpp
+++ 03/main.cpp
@@ -3,56 +3,56 @@
 #include "matrix.h"
 
 int main() {
-	Matrix m(2, 9);
-	m[0][2] = 4;
-	if (m[0][2] == 4)
-		printf("Test 1 passed ([][])\n");
+    Matrix m(2, 9);
+    m[0][2] = 4;
+    if (m[0][2] == 4)
+        printf("Test 1 passed ([][])\n");
 
-	m *= 10000;
-	if (m[0][2] == 40000)
-		printf("Test 2 passed (*=)\n");
+    m *= 10000;
+    if (m[0][2] == 40000)
+        printf("Test 2 passed (*=)\n");
 
-	try {
-		Matrix n(1, -1);
-	}
-	catch(std::bad_alloc& ba) {
-		printf("Test 3 passed (bad alloc 1)\n");
-	}
+    try {
+        Matrix n(1, -1);
+    }
+    catch(std::bad_alloc& ba) {
+        printf("Test 3 passed (bad alloc 1)\n");
+    }
 
-	try {
-		Matrix n(-1, 1);
-	}
-	catch(std::bad_alloc& ba) {
-		printf("Test 4 passed (bad alloc 2)\n");
-	}
+    try {
+        Matrix n(-1, 1);
+    }
+    catch(std::bad_alloc& ba) {
+        printf("Test 4 passed (bad alloc 2)\n");
+    }
 
-	const Matrix cm(2, 3);
-	if (!cm[1][1] || cm[1][1])
-		printf("Test 5 passed (const matrix[][])\n");
+    const Matrix cm(2, 3);
+    if (!cm[1][1] || cm[1][1])
+        printf("Test 5 passed (const matrix[][])\n");
 
-	if (cm.getRows() == 2 && cm.getCols() == 3)
-		printf("Test 6 passed (getRows and getCols)\n");
+    if (cm.getRows() == 2 && cm.getCols() == 3)
+        printf("Test 6 passed (getRows and getCols)\n");
 
-	Matrix dm(2, 2);
-	for (int i = 0; i < 2; ++i)
-		for (int j = 0; j < 2; ++j)
-			dm[i][j] = cm[i][j];
+    Matrix dm(2, 3);
+    for (int i = 0; i < 2; ++i)
+        for (int j = 0; j < 3; ++j)
+            dm[i][j] = cm[i][j];
 
-	if (dm == cm)
-		printf("Test 7 passed (==)\n");
+    if (dm == cm)
+        printf("Test 7 passed (==)\n");
 
-	dm[0][1] = !cm[0][1];
-	if (dm != cm)
-		printf("Test 8 passed (!=)\n");
+    dm[0][1] = !cm[0][1];
+    if (dm != cm)
+        printf("Test 8 passed (!=)\n");
 
-	try {
-		m[0][10000] = 7;
-	}
-	catch(std::out_of_range& oor) {
-		if (strcmp(oor.what(), "Out of range in j"))
-			printf("Test 9 passed (out of range)\n");
-	}
+    try {
+        m[0][10000] = 7;
+    }
+    catch(std::out_of_range& oor) {
+        if (strcmp(oor.what(), "Out of range in j"))
+            printf("Test 9 passed (out of range)\n");
+    }
 
-	printf("All 9 tests passed, success.\n");
-	return 0;
+    printf("All 9 tests passed, success.\n");
+    return 0;
 }
--- 03/matrix.cpp
+++ 03/matrix.cpp
@@ -1,80 +1,84 @@
 #include "matrix.h"
 
 Matrix::Row::Row(std::size_t c) {
-	cols = c;
-	A = new int[cols];
+    cols = c;
+    A = new int[cols];
 }
 
 const int& Matrix::Row::operator[](std::size_t j) const {
-	if (j >= cols)
-		throw std::out_of_range("Out of range error in j");
-	return A[j];
+    if (j >= cols)
+        throw std::out_of_range("Out of range error in j");
+    return A[j];
 }
 
 int& Matrix::Row::operator[](std::size_t j) {
-	if (j >= cols)
-		throw std::out_of_range("Out of range error in j");
-	return A[j];
+    if (j >= cols)
+        throw std::out_of_range("Out of range error in j");
+    return A[j];
 }
 
 Matrix::Row::~Row() {
-	free(A);
+    delete A;
 }
 
 Matrix::Matrix(std::size_t r, std::size_t c) {
-	Matrix::Row* ptr;
-	rows = r, cols = c;
+    Matrix::Row* ptr;
+    rows = r, cols = c;
 
-	B = new Row*[rows];
+    B = new Row*[rows];
 
-	for (std::size_t i = 0; i < rows; ++i) {
-		try {
-			ptr = new Matrix::Row(c);
-		}
-		catch(std::bad_alloc& ba) {
-			delete []B;
-			throw ba;
-		}
-		B[i] = ptr;
-	}
+    for (std::size_t i = 0; i < rows; ++i) {
+        try {
+            ptr = new Matrix::Row(c);
+        }
+        catch(std::bad_alloc& ba) {
+            for (int j = 0; j < i; ++j) {
+                printf("I am here\n");
+                delete B[j];
+            }
+            delete B;
+            throw ba;
+        }
+        B[i] = ptr;
+    }
 }
 
 std::size_t Matrix::getRows() const {
-	return rows;
+    return rows;
 }
 
 std::size_t Matrix::getCols() const {
-	return cols;
+    return cols;
 }
 
 const Matrix::Row& Matrix::operator[](std::size_t i) const {
-	if (i >= rows)
-		throw std::out_of_range("Out of range error in i");
-	return *B[i];
+    if (i >= rows)
+        throw std::out_of_range("Out of range error in i");
+    return *B[i];
 }
 
 Matrix::Row& Matrix::operator[](std::size_t i) {
-	if (i >= rows)
-		throw std::out_of_range("Out of range error in i");
-	return *B[i];
+    if (i >= rows)
+        throw std::out_of_range("Out of range error in i");
+    return *B[i];
 }
 
 void Matrix::operator*=(int d) {
-	for (std::size_t i = 0; i < rows; ++i)
-    	for (std::size_t j = 0; j < cols; ++j)
-        	(*this)[i][j] *= d;
+    for (std::size_t i = 0; i < rows; ++i)
+        for (std::size_t j = 0; j < cols; ++j)
+            (*this)[i][j] *= d;
 }
 
 bool Matrix::operator==(const Matrix& other) const {
     if (this == &other)
         return true;
     if (other.getCols() != cols || other.getRows() != rows)
-    	return false;
+        return false;
 
     for (std::size_t i = 0; i < rows; ++i)
-    	for (std::size_t j = 0; j < cols; ++j)
-        	if ((*this)[i][j] != other[i][j])
-        		return false;
+        for (std::size_t j = 0; j < cols; ++j)
+            if ((*this)[i][j] != other[i][j])
+                return false;
 
     return true;
 }
@@ -84,5 +88,7 @@ bool Matrix::operator!=(const Matrix& other) const {
 }
 
 Matrix::~Matrix() {
-	delete []B;
+    for (int j = 0; j < rows; ++j)
+        delete B[j];
+    delete B;
 }
