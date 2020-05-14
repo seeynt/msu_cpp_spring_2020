68d4dd04451e6a1b2d3e0c3873b8d1ad428f69e4
seeynt <61554700+seeynt@users.noreply.github.com>
2020-05-14 20:52:29 +0300

    Add files via upload

--- exam/sum.cpp
+++ exam/sum.cpp
@@ -1,35 +1,72 @@
 #include "sum.h"
 
 static int result = 0;
+static bool stop = false;
+static std::mutex mutex_;
+static std::condition_variable cv;
+static std::queue<std::string> lines;
 
-static int sum_line(const std::string line) {
-	std::stringstream ss(line);
-	std::mutex mutex_;
+static void sum_line() {
+	std::string line;
+	std::istringstream ss;
 	int sum = 0, val;
-	while (ss >> val)
-		sum += val;
+	while (true) {
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+        	cv.wait(lock, []() { return !lines.empty() || stop; });
+
+            if (stop)
+                return;
+
+            line = std::move(lines.front());
+            lines.pop();
+        }
+
+		ss.str(line);
+		sum = 0;
+
+		while (ss >> val)
+			sum += val;
+
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			result += sum;
+		}
+	}
+}
+
+static void read_file(const std::string& fname) {
+	std::ifstream fin(fname);
+    std::string line;
+
+	while (std::getline(fin, line)) {
+		std::unique_lock<std::mutex> lock(mutex_);
+		lines.push(line);
+		cv.notify_one();
+	}
 
 	{
 		std::unique_lock<std::mutex> lock(mutex_);
-		result += sum;
-		sum = result;
+		stop = true;
+		cv.notify_all();
 	}
 
-	return sum;
+	return;
 }
 
-int sum_file(const std::string& fname) {
-	std::string line;
-	result = 0;
-	int ans = 0;
-	ThreadPool pool(2);
+int sum_file(std::string fname) {
+	result = 0, stop = false;
+	std::vector<std::thread> sum_thread;
 
-	std::ifstream fin("test.txt");
-	while (std::getline(fin, line)) {
-		auto task = pool.exec(sum_line, line);
-		ans = task.get();
-	}
+	std::thread read_thread(read_file, fname);
 
-	std::cout << ans << std::endl;
-	return ans;
+	for (int i = 0; i < 2; ++i)
+		sum_thread.emplace_back(sum_line);
+
+	read_thread.join();
+	sum_thread[0].join();
+	sum_thread[1].join();
+
+	std::cout << result << std::endl;
+	return result;
 }
